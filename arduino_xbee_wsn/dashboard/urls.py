aa2a7436e58c3a1b3c43801bd5cc9d5d33fd705b
aurorapapotti <aurora.papotti@studenti.unitn.it>
2020-11-07 17:43:12 +0200

    Dashboard for each sensor

--- arduino_xbee_wsn/dashboard/urls.py
+++ arduino_xbee_wsn/dashboard/urls.py
@@ -3,4 +3,5 @@ from dashboard import views
 
 urlpatterns = [
     path('dashboard/', views.charts, name='charts'),
+    path('dashboard/<int:sensor_id>/', views.charts_detail, name='charts_detail'),
 ]
